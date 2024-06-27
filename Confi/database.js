a2d3066701d8db6795566c7ac0ba5da05a08d158
KareMartinez035 <masilka.km@gmail.com>
2024-06-27 13:04:45 -0500

    27-junio

--- Confi/database.js
+++ Confi/database.js
@@ -1,10 +1,9 @@
 const db={
     host:'viaduct.proxy.rlwy.net',
-    port:31099,
+    port:52313,
     user:"root",
-    password:"tiRaXujnXuAiFUHvaKAijUsjQiUYpXmX",
-    database:'railway',
-    url:'mysql://root:tiRaXujnXuAiFUHvaKAijUsjQiUYpXmX@viaduct.proxy.rlwy.net:31099/railway'
+    password:"IuLygmdgtQSHfwIQvrTdyxPZczbrNGnO",
+    database:'railway'
 }
 
 export default db
